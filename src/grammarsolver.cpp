05c7eed8c51f8e836ff9f8eb24d0067fe8225d36
Den Chan <98968275+denwychan@users.noreply.github.com>
2022-09-07 12:49:37 +0100

    Started program decomposition

--- src/grammarsolver.cpp
+++ src/grammarsolver.cpp
@@ -1,11 +1,36 @@
 // This is the CPP file you will edit and turn in. (TODO: Remove this comment!)
 
 #include "grammarsolver.h"
+#include "strlib.h"
+#include "map.h"
+#include "vector.h"
+
 using namespace std;
 
 Vector<string> grammarGenerate(istream& input, string symbol, int times) {
-    // TODO: write this function
+
+    Map<string, Vector<string> > map;
+
+    // readInputFile() read an input file with a grammar in Backus-Naur Form
+    // and turns its contents into a data structure
+
+    // generate elements randomly generate elements of the grammar (recursively)
     
+    // Use trim to get rid of white space
+
     Vector<string> v;   // this is only here so it will compile
     return v;           // this is only here so it will compile
+}
+
+void readInputFile(){
+    // read file
+
+    // extract key and extract values for a map
+    // if values have no white space, put into vector of strings
+    // if values have space, put into nested vector (vector inside a vector
+    // of strings
+
+    // stringSplit
+    // Vector<string> stringSplit(string s, string delimiter)
+
 }
